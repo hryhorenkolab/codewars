8028dc115264c05a2933b05a564cdbb6dd33c57e
hryhorenkolab <64160375+hryhorenkolab@users.noreply.github.com>
2022-02-23 00:23:42 +0100

    Update Convert number to reversed array of digits.py

--- Convert number to reversed array of digits.py
+++ Convert number to reversed array of digits.py
@@ -1,5 +1,5 @@
 def digitize(n):
-  """Given a random non-negative number, you have to return the digits of this number within an array in reverse order. """
+  """Given a random non-negative number, you have to return the digits of this number within an array in reverse order. 348597 => [7,9,5,8,4,3] """
     num_array = [] # make empty array
     for number in str(n): # make n into string to interate through 
         num_array.insert(0, int(number)) # insert number into 1st position (reverses it)
